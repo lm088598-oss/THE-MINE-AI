1492b79bdb1cd96cf3f7086964fa828ff5a40cf3
lm088598-oss <lm088598@gmail.com>
2026-02-24 19:16:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # --- ඔයාගේ API Key එක පහළ ඇති quotation marks ඇතුළට දාන්න ---
-API_KEY = "AIzaSyBmlbUS2TmfPKYhNVF..." # ඔයාගේ කලින් තිබුණ Key එක මෙතන තියෙයි
+API_KEY = "AIzaSyBmlbUS2TmfPKYhNVPJekL1RhoaXE70X7c" # ඔයාගේ කලින් තිබුණ Key එක මෙතන තියෙයි
 
 genai.configure(api_key=API_KEY)
 
